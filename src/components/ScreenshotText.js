c7fc9f825a38f4d369da6ea955b14ed9d5f8c046
Denys <denv928@gmail.com>
2018-06-23 18:07:03 +0300

    Update

--- src/components/ScreenshotText.js
+++ src/components/ScreenshotText.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react'
+import React, {Component,cloneElement} from 'react'
 import html2canvas from 'html2canvas'
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 
 
 class ScreenshotText extends Component {
@@ -9,15 +9,15 @@ class ScreenshotText extends Component {
         super(props);
         this.elementText = React.createRef();
         this.state = {
-            elementImg:null
+            elementImg: null
         }
     }
 
     doScreenshots() {
         html2canvas(this.elementText.current).then(canvas => {
-            this.setState(()=>{
+            this.setState(() => {
                 return {
-                    elementImg: canvas // Повертає DOM елемент
+                    elementImg: canvas.toDataURL('img/jpg   ') // Повертає DOM елемент
                 }
             })
         }).catch((e) => {
@@ -27,16 +27,14 @@ class ScreenshotText extends Component {
 
     render() {
         if (this.state.elementImg) {
-            return this.state.elementImg
+            return <img src={this.state.elementImg} width='100%' alt='Текст питання'/>
         } else {
             return <p ref={this.elementText}>{this.props.text}</p>
         }
     }
 
     componentDidMount() {
-        this.setState(() => {
-            return {didScreenshot: true}
-        })
+        this.doScreenshots()
     }
 
 }
